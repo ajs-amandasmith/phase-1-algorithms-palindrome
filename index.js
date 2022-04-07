ca140b14370fa6744d363d03a2ab6e3dcf190eee
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-07 16:55:52 -0500

    commit tree a780c5e1c16431f3320e3ce69626c8182cf5d063

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordReverseArray = [];
+  for (let i = word.length; i >= 0; i--) {
+    wordReverseArray.push(word[i]);
+  }
+  const newWord = wordReverseArray.join('');
+  if (newWord === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome(string) {
+    add new string variable
+    iterate over the input string in reverse
+      add each letter to the new string variable
+        if the new string is the same as the input string
+          return true
+        else
+          return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+  function isPalindrome will take in one word, which is a string
+  it should return true if the input word is a palindrome and false if it isn't
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
